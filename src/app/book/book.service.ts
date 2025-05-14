98cf118a0a88370dbff95cdad90f14bb8cd4e833
Juan <je.briceno2@uniandes.edu.co>
2025-05-13 22:22:49 -0500

    Estudio COMPLETADO LES GO

---
+++ src/app/book/book.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+private apiUrl = environment.baseUrl + 'books';
+constructor(private http: HttpClient) { }
+
+getBooks(): Observable<Book[]> {
+  return this.http.get<Book[]>(this.apiUrl);
+
+ }
+}
